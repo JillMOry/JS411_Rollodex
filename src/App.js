5b30907ba3d2603b034d9bb10734222caf934110
JillMOry <oryjill@gmail.com>
2020-05-31 18:28:12 -0500

    rollodex commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class User extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			withDetails: false
+		};
+	}
+
+	handleClick = () => {
+		this.setState({
+			withDetails: !this.state.withDetails
+		});
+	};
+
+	render() {
+		let details;
+
+		if (!this.state.withDetails) {
+			details = (
+				<div className="App">
+					<img
+						style={{ width: "60px" }}
+						src={this.props.user.picture.thumbnail}
+						alt="contact person"
+					></img>
+					<span>
+						{this.props.user.name.first} {this.props.user.name.last}
+					</span>
+				</div>
+			);
+		} else {
+			details = (
+				<div className="App2">
+					<img
+						style={{ width: "60px" }}
+						src={this.props.user.picture.thumbnail}
+						alt="contact person"
+					></img>
+
+					<span>
+						{this.props.user.name.first} {this.props.user.name.last}
+						<br></br>
+						{this.props.user.location.street.number} {}
+						{this.props.user.location.street.name}
+						<br></br>
+						{this.props.user.location.city}, {this.props.user.location.state} {}
+						{this.props.user.location.postcode}
+						<br></br>
+						{this.props.user.email}
+						<br></br>
+						{this.props.user.cell}
+					</span>
+				</div>
+			);
+		}
+		return (
+			<div className="App" id="container">
+				<div>{details}</div>
+
+				<button style={{ fontSize: "10px" }} onClick={this.handleClick}>
+					{this.state.withDetails ? "Hide Details" : "Show Details"}
+				</button>
+			</div>
+		);
+	}
 }
 
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://randomuser.me/api?results=25")
+			.then((json) => json.json())
+			.then((data) => {
+				this.setState({
+					users: data.results
+				});
+				console.log(data);
+			})
+			.catch((error) => console.log("parsing failed", error));
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.users.map((userData, index) => (
+					<User key={index} user={userData} />
+				))}
+			</div>
+		);
+	}
+}
 export default App;
